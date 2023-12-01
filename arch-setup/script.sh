d57b5e667e4cc5ebcaff15461f3c2c8acd07db88
pops <115036828+POP303U@users.noreply.github.com>
2023-12-01 12:06:22 +0100

    Update: Clean up in install script

--- arch-setup/script.sh
+++ arch-setup/script.sh
@@ -1,1 +1,8 @@
-sudo pacman -Sy p7zip unrar tar rsync github-cli git neofetch htop exfat-utils fuse-exfat ntfs-3g flac jasper aria2 curl wget jp2a less --noconfirm
+sudo pacman -Sy p7zip unrar tar --noconfirm
+sudo pacman -Sy rsync github-cli git --noconfirm
+sudo pacman -Sy exfat-utils fuse-exfat ntfs-3g --noconfirm
+sudo pacman -Sy flac jasper aria2  less --noconfirm
+sudo pacman -Sy neofetch htop curl wget jp2a --noconfirm
+sudo pacman -Sy pipewire alsa-utils pipewire-pulse --noconfirm
+sudo pacman -Sy NetworkManager network-manager-applet --noconfirm
+sudo pacman -Sy lxsession lxappearance --noconfirm
